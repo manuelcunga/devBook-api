cc3f9469d043bd8ba76d60dc7fc2c7d1effe60d0
Lázaro Manuel <manuelvianacunga@gmail.com>
2022-08-25 15:21:28 +0100

    feat: :sparkles: adicionando user entities

---
+++ src/entities/User.go
@@ -0,0 +1,12 @@
+package entities
+
+import "time"
+
+type User struct {
+	ID        uint64    `json:"id, omitempty"`
+	Name      string    `json:"name, omitempty"`
+	Nick      string    `json:"nick, omitempty"`
+	Email     string    `json:"email, omitempty"`
+	Password  string    `json:"password, omitempty"`
+	CreatedAt time.Time `json:"createdAt, omitempty"`
+}
